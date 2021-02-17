7a4516107b492b103da1cb87a8a84b10b153b5bd
0980991 <0980991@users.noreply.github.com>
2021-02-17 19:41:14 +0100

    First Commit
    + Bubble sort  (slow)
    + Bubble sort (optimized)
            + Decreases iteration by 1 after every loop

---
+++ Sample Algos/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace Sample_Algos
+{
+    class Program
+    {
+        public static int GenerateLength() //Generate a random integer between 2 - 40 to use as the length of our array
+        {
+            
+            System.Random random = new System.Random();
+            return random.Next(2, 40);
+        }
+        public static int[] CreateArray(int length) //Creates an array
+        {
+            int[] sequence = new int[length];
+            for(int i = 0; i < sequence.GetLength(0); i++)
+            {   
+                sequence[i] = i + 1;
+            }
+            return sequence;
+        }
+        public static int[] BubbleSort(int[] sequence) //Bubble sorts an array from largest to smallest
+        {
+            bool isSorted = false;
+            while(isSorted == false)
+            {
+                int counter = 0; //counts the amount of times 2 numbers swap to determine whether the sorting is completed by informing 'isSorted'
+
+                for(int i = 0; i < sequence.Length; i++)
+                {   
+                    if(i != sequence.Length - 1){ //Prevents exception errors with [i+1]
+                        if(sequence[i] < sequence[i+1])
+                        {
+                            int temp = sequence[i];
+                            sequence[i] = sequence[i+1];
+                            sequence[i+1] = temp;
+                            counter++;                                 
+                        }                        
+                    }
+                }
+                if(counter == 0)  //Have their been any swaps this round?
+                {
+                    isSorted = true; 
+                }
+            }
+            return sequence;
+        }
+        public static int[] BubbleSortFaster(int[] sequence) //Bubble sorts an array from largest to smallest
+        {
+             bool isSorted = false;
+            while(isSorted == false)
+            {
+                int counter = 0; //counts the amount of times 2 numbers swap to determine whether the sorting is completed by informing 'isSorted'
+
+                for(int i = 0; i < sequence.Length; i++)
+                {   
+                    if(i != sequence.Length - 1){ //Prevents exception errors with [i+1]
+                        if(sequence[i] < sequence[i+1])
+                        {
+                            int temp = sequence[i];
+                            sequence[i] = sequence[i+1];
+                            sequence[i+1] = temp;
+                            counter++;                                 
+                        }                        
+                    }
+                }
+                if(counter == 0)  //Have their been any swaps this round?
+                {
+                    isSorted = true; 
+                }
+            }
+            return sequence;
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            int [] sequence = CreateArray(GenerateLength());
+            
+            for(int i = 0; i < sequence.GetLength(0); i++)
+                {   
+                    //Console.WriteLine(sequence[i]);
+                    Console.WriteLine(BubbleSort(sequence)[i]);
+                    //Console.WriteLine(BubbleSortFaster(sequence)[i]);
+                }       
+
+        }
+    }
+}
